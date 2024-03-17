6728a3e350734ecb93c20524786e28b7b0fad5e1
Matheus Leonardo Teixeira <151759710+HoullynBR@users.noreply.github.com>
2024-03-17 09:36:22 -0300

    Controle.py

--- Controle.py
+++ Controle.py
@@ -4,29 +4,29 @@ import getpass
 lista = []
 
 while True:
-        user = "Matheus"
-        key = 12345
-        licensa = 30
-        login = str(input("\n !#!#!#!    Digite seu usuario: "))
-        senha = float(input("\n !#!#!#!    Digite sua senha: "))
-        print("\n       ### Aguarde... ### ")
-        time.sleep(3)
-        if login == user and senha == key:
-            print(f"\n !#!#!#! Login efetuado com sucesso! !#!#!#!\n  !#!#! sua licensa expira em {licensa} dias !#!#!")
-        else:
-            print(f"\n !#!#!#! Senha e/ou Usuario incorretos\n !#!#!#! Deseja tentar acessar novamente?  !#!#!#!\n\n")
-        break
-while True:
+    user = "Matheus"
+    key = 12345
+    licensa = 30
+    login = str(input("\n !#!#!#!    Digite seu usuario: "))
+    senha = float(input("\n !#!#!#!    Digite sua senha: "))
+    print("\n       ### Aguarde... ### ")
+    time.sleep(3)
     if login == user and senha == key:
-        print(f"\n       !#!#!#! Menu Inicial !#!#!#!\n\n  !#!#!#! [1]  Adicionar item     !#!#!#!\n  !#!#!#! [2]  Mostar item      !#!#!#!  \n  !#!#!#! [3]  Editar item        !#!#!#!\n  !#!#!#! [4]  Remover item       !#!#!#!\n  !#!#!#! [5]  Exibir todos itens !#!#!#!\n")
-    opcao = int(input("Selecione a opção: "))
+        print(f"\n !#!#!#! Login efetuado com sucesso! !#!#!#!\n  !#!#! sua licensa expira em {licensa} dias !#!#!")
+    else:
+        print(f"\n !#!#!#! Senha e/ou Usuario incorretos  !#!#!#!\n !#!#!#! Deseja tentar acessar novamente?  !#!#!#!\n\n")
+        break
+    
+    if login == user and senha == key:
+            print(f"\n       !#!#!#! Menu Inicial !#!#!#!\n\n  !#!#!#! [1]  Adicionar item     !#!#!#!\n  !#!#!#! [2]  Mostar itens       !#!#!#!\n  !#!#!#! [3]  Sair               !#!#!#!\n\n")
+    opcao = int(input("!#!#!#!  Selecione a opção: "))
     if opcao == 1:
-        adicionar = input("Adicionar item: ")
-        lista.append(adicionar)	
+            adicionar = input("!#!#!#!  Adicionar item: ")
+            lista.append(adicionar)	
     elif opcao == 2:
-        for item in lista:
-            print (f"\n item: {item}")
+            for item in lista:
+                print (f"\n item: {item}")
     elif opcao == 3:
         break
     else:
-            print("Opção Inválida!")
+            print("!#!#!#!  Opção Inválida!       !#!#!#!")
