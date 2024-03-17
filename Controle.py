03b7904db0b02b4fc35f560d2999e90156778be7
Matheus Leonardo Teixeira <151759710+HoullynBR@users.noreply.github.com>
2024-03-17 08:42:19 -0300

    Controle.py

--- Controle.py
+++ Controle.py
@@ -4,21 +4,23 @@ import getpass
 lista = []
 
 while True:
-	user = "Matheus"
-	key = 12345
-	licensa = 30
-	login = str(input("\n !#!#!#!    Digite seu usuario: "))
-	senha = float(input("\n !#!#!#!    Digite sua senha: "))
-	print("\n       ### Aguarde... ### ")
-	time.sleep(3)
-	if login == user and senha == key:
-		print(f"\n !#!#!#! Login efetuado com sucesso! !#!#!#!\n  !#!#! sua licensa expira em {licensa} dias !#!#!")
-	else:
-		print(f"\n !#!#!#! Senha e/ou Usuario incorretos\n !#!#!#! Deseja tentar acessar novamente?  !#!#!#!\n\n")
-	break
-	while True:
-         if login == user and senha == key:				print(f"\n       !#!#!#! Menu Inicial !#!#!#!\n\n  !#!#!#! [1]  Adicionar item     !#!#!#!\n  !#!#!#! [2]  Procurar item      !#!#!#!  \n  !#!#!#! [3]  Editar item        !#!#!#!\n  !#!#!#! [4]  Remover item       !#!#!#!\n  !#!#!#! [5]  Exibir todos itens !#!#!#!\n")
-            opcao = int(input("Selecione a opção: "))
-     	if opcao == 1:
-  	adicionar = input("Adicionar item: ")
-  	lista.append(adicionar)	
+        user = "Matheus"
+        key = 12345
+        licensa = 30
+        login = str(input("\n !#!#!#!    Digite seu usuario: "))
+        senha = float(input("\n !#!#!#!    Digite sua senha: "))
+        print("\n       ### Aguarde... ### ")
+        time.sleep(3)
+        if login == user and senha == key:
+            print(f"\n !#!#!#! Login efetuado com sucesso! !#!#!#!\n  !#!#! sua licensa expira em {licensa} dias !#!#!")
+        else:
+            print(f"\n !#!#!#! Senha e/ou Usuario incorretos\n !#!#!#! Deseja tentar acessar novamente?  !#!#!#!\n\n")
+        break
+while True:
+    if login == user and senha == key:
+        print(f"\n       !#!#!#! Menu Inicial !#!#!#!\n\n  !#!#!#! [1]  Adicionar item     !#!#!#!\n  !#!#!#! [2]  Procurar item      !#!#!#!  \n  !#!#!#! [3]  Editar item        !#!#!#!\n  !#!#!#! [4]  Remover item       !#!#!#!\n  !#!#!#! [5]  Exibir todos itens !#!#!#!\n")
+    opcao = int(input("Selecione a opção: "))
+    if opcao == 1:
+        adicionar = input("Adicionar item: ")
+        lista.append(adicionar)	
+    elif
