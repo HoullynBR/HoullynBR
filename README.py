22a88564b2df6bbe279fac6fc60cef9c53fc0cff
Matheus Leonardo Teixeira <151759710+HoullynBR@users.noreply.github.com>
2026-04-27 10:26:00 -0300

    Revise README to simplify personal information
    Removed personal introduction and project details.

--- README.py
+++ README.py
@@ -2,27 +2,6 @@
 👨‍💻 Desenvolvedor Jr. em Python | 🔧 Automação & Bots | ☁️ Fundador da Houllyn Systems
 ⚽ Apaixonado por futebol, tecnologia, liberdade digital e projetos que transformam ideias em soluções reais.
 
-Sou desenvolvedor júnior especializado em Python, com foco em automações, bots, APIs e soluções inteligentes.
-Fundador da  Houllyn Systems, atuo no desenvolvimento de softwares,robôs e sistemas que unem performance, inteligência e escalabilidade.
-
-💡 Projetos em andamento:
-🤖 Houllyn Action Trader (bot com IA para operações financeiras automatizadas)
-📲 10eFaixa Academy (aplicativo de gestão e recompensa para atletas amadores)
-🌐 Houllynet (provedor alternativo de internet via aplicativo)
-⚙️ Robôs com Arduino e Raspberry Pi (automações físicas e educacionais)
-
-
-🚀 Skills e stack:
-Python, API REST, Web Scraping, Telegram Bots, Tkinter, PyQt
-Integrações com Corretoras digitais, Firebase, Firestore, SQLite, e automações em tempo real
-Conhecimentos em redes, servidores SSH, proxy, e scripts de automação
-Interesse crescente em C/C++, machine learning e projetos embarcados
-
-
-🎯 Visão:
-Unir tecnologia, acessibilidade e liberdade criativa em soluções digitais com propósito. 
-Acredito na inovação como ponte entre o mundo real e o digital, promovendo autonomia e oportunidades.
-
 📫 Me acompanhe ou entre em contato:
 ➡️ GitHub: github.com/HoullynBR
 ➡️ Instagram: @mthstxra
