6a467bbde9e2c9d813f562d4f4007b11caed301c
Matheus Leonardo Teixeira <151759710+HoullynBR@users.noreply.github.com>
2026-08-26 12:37:03 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,11 +1,4 @@
 👋 Olá, eu sou Matheus Leonardo Teixeira
-👨‍💻 Desenvolvedor Jr. em Python | 🔧 Automação & Bots | ☁️ Fundador da Houllyn Systems
-⚽ Apaixonado por futebol, tecnologia, liberdade digital e projetos que transformam ideias em soluções reais.
-
-📫 Me acompanhe ou entre em contato:
-➡️ GitHub: github.com/HoullynBR
-➡️ Instagram: @mthstxra
-➡️ Empresa: @houllyn.cloud
 
 <!---
 HoullynBR/HoullynBR is a ✨ special ✨ repository because its `README.py` (this file) appears on your GitHub profile.
